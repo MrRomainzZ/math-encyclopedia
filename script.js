3cc05e721ae2aa9fda7646280bce9c76a136619b
RomainzZ <roman.shalin87@gmail.com>
2022-10-12 01:18:40 +0800

    Started to implement JavaScript - Info button is now working - Added grid for background

---
+++ script.js
@@ -0,0 +1,7 @@
+let infopanel = document.querySelector("#info-panel");
+
+let btninfo = document.querySelector("#btninfo");
+
+btninfo.addEventListener('click', function(smth) {
+	infopanel.toggleAttribute("Hidden")
+});
